baea627f30728b72f560928076568b9a12ec980e
Artem Zhyrnyi <ic3ego@gmail.com>
2025-06-18 12:19:06 +0300

    minor code refactoring

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Price from './Price';
 import type ProductType from '../types/ProductType';
@@ -8,12 +9,20 @@ interface Props {
 }
 
 const Product = ({ details }: Props) => {
+  const [loaded, setLoaded] = useState(false);
   const { id, thumbnail, name, final_price, original_price } = details;
 
   return (
     <div className="product">
       <Link to={`/products/${id}`}>
-        <img width="272" height="300" src={thumbnail} alt={name} />
+        <img
+          width="200"
+          height="220"
+          src={thumbnail}
+          alt={name}
+          className={loaded ? 'loaded' : ''}
+          onLoad={() => setLoaded(true)}
+        />
         <h2>{name}</h2>
         <div className="product__price">
           <Price finalPrice={final_price} originalPrice={original_price} />
