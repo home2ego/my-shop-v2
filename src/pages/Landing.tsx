ab54d2f2291a5fc3ff70dc5d8f144d37ab932b1a
Artem Zhyrnyi <ic3ego@gmail.com>
2025-06-18 22:19:10 +0300

    minore code refactoring

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,7 +4,6 @@ import './Landing.css';
 const Landing = () => {
   return (
     <>
-      <title>MyShop</title>
       <h1>Online shopping simplified</h1>
       <p className="landing-tagline text-dimmed">
         Order your groceries from MyShop with our easy to use app, and get your
@@ -19,7 +18,7 @@ const Landing = () => {
         <img
           width="400"
           height="400"
-          src="landing.svg"
+          src="/landing.svg"
           alt="Shopping cart display with purchases"
           fetchPriority="high"
         />
