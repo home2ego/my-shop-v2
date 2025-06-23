ef75121a0ed3b878c80fd08cdfdf196eb5f409a1
Artem Zhyrnyi <ic3ego@gmail.com>
2025-06-23 21:51:36 +0300

    minor code refactoring

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { ViteMinifyPlugin } from "vite-plugin-minify";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vite.dev/config/
 export default defineConfig({
